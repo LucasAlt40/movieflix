e572ceeaefd4c93afc034591aa37e66d56a3a2b1
LucasAlt40 <lucas.alt40@gmail.com>
2021-11-14 23:26:29 -0300

    Testando requisição da api e adicionando sass ao projeto

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import "./App.scss";
 
 function App() {
+  const [filme, setFilme] = useState("");
+
   useEffect(() => {
     async function fetchData() {
       const response = await fetch(
         `https://api.themoviedb.org/3/movie/550?api_key=${process.env.REACT_APP_API_KEY}`
       );
       const data = await response.json();
-      console.log(data.original_title);
+      setFilme(data.original_title);
     }
     fetchData();
   }, []);
@@ -15,6 +18,10 @@ function App() {
   return (
     <div>
       <h1>Hello World!</h1>
+      <h2>The film who everyone chose is...</h2>
+      <h3>
+        The <strong>{filme}</strong>!
+      </h3>
     </div>
   );
 }
