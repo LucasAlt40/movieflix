b5d4afa10f389abdfcca546a50269d32d6914dd8
LucasAlt40 <lucas.alt40@gmail.com>
2021-11-14 23:14:58 -0300

    testando requisição na api de filmes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/550?api_key=${process.env.REACT_APP_API_KEY}`
+      );
+      const data = await response.json();
+      console.log(data.original_title);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h1>Hello World!</h1>
