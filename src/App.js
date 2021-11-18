08748271057a7cf56ee243c1da10eadaa1693b40
LucasAlt40 <lucas.alt40@gmail.com>
2021-11-18 16:36:21 -0300

    testando requisições

--- src/App.js
+++ src/App.js
@@ -1,27 +1,64 @@
+/* eslint-disable */
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.scss";
 
 function App() {
-  const [filme, setFilme] = useState("");
+  const [filme, setFilme] = useState({});
+  const [nomeFilme, setNomeFilme] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [filmId, setFilmId] = useState("");
+
+  const api_key = process.env.REACT_APP_API_KEY;
+
+  async function fetchApi() {
+    await axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${nomeFilme}&language=pt-Br`
+      )
+      .then((response) => {
+        setFilmId(response.data.results[0].id);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    await axios
+      .get(`https://api.themoviedb.org/3/movie/${filmId}?api_key=${api_key}&language=pt-Br`)
+      .then((response) => {
+        setFilme(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   useEffect(() => {
-    async function fetchData() {
-      const response = await fetch(
-        `https://api.themoviedb.org/3/movie/550?api_key=${process.env.REACT_APP_API_KEY}`
-      );
-      const data = await response.json();
-      setFilme(data.original_title);
-    }
-    fetchData();
-  }, []);
+    fetchApi();
+  }, [nomeFilme]);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
 
   return (
     <div>
-      <h1>Hello World!</h1>
-      <h2>The film who everyone chose is...</h2>
-      <h3>
-        The <strong>{filme}</strong>!
-      </h3>
+      <label>Nome do filme:</label>
+      <input
+        onChange={(e) => {
+          setNomeFilme(e.target.value);
+        }}
+        type="text"
+      />
+      <hr />
+      <h2>O filme escolhido foi...</h2>
+      <h3>{filme.title}</h3>
+      <img
+        src={`https://image.tmdb.org/t/p/w500/${filme.poster_path}`}
+        alt=""
+      />
     </div>
   );
 }
